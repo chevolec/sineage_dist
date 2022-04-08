cf59088bf421a40c4ffca725913948322db9eec0
chevolec <chevolec>
2022-04-08 12:31:34 -0500

    Add display driver

--- install.sh
+++ install.sh
@@ -8,10 +8,15 @@ cd ~
 
 # download latest version of project
 git clone https://github.com/chevolec/sineage_dist.git
+git clone https://github.com/goodtft/LCD-show.git
 
 mv sineage_dist/* ./
 mv bashrc .bashrc
 rm -fr sineage_dist
 
 chmod +x sineage_dist.sh
+
+chmod -R 755 LCD-show
+cd LCD-show
+sudo ./LCD5-show
 
